0923880a6020f19cd0d550edadf4de29f68027fc
yesmin <ymslahoud@gmail.com>
2020-06-19 20:50:57 -0300

    conceitos-nodejs

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ app.post("/repositories", (request, response) => {
 
   const repository = {
     id: uuid(),
-    title, 
-    url, 
+    title,
+    url,
     techs,
     likes: 0,
   }
@@ -37,8 +37,8 @@ app.put("/repositories/:id", (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id)
 
-  if(repositoryIndex < 0){
-    return response.status(400).json({ error: 'Repository not found.'})
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
   }
 
   const repository = {
@@ -46,6 +46,7 @@ app.put("/repositories/:id", (request, response) => {
     title,
     url,
     techs,
+    likes: 0,
   };
 
   repositories[repositoryIndex] = repository;
@@ -59,7 +60,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if(repositoryIndex < 0){
+  if (repositoryIndex < 0) {
     return response.status(400).json({ error: 'Repository not found.' });
   }
 
@@ -69,11 +70,11 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  const {id} = request.params;
+  const { id } = request.params;
 
   const repository = repositories.find(repository => repository.id === id)
 
-  if(!repository) {
+  if (!repository) {
     return response.status(400).send();
   }
 
